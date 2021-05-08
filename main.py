1fe0f9e31dffaa7b0d55cc1d348dac52d189433b
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-05-08 19:55:44 +0530

    code

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ pdf_reader = PyPDF2.PdfFileReader(book)
 num_pages = pdf_reader.numPages
 
 play = pyttsx3.init()
-print('Playing Audio Book')
+print('Started Playing Audio Book')
 
 for num in range(0, num_pages):
     page = pdf_reader.getPage(num)
     data = page.extractText()
     play.say(data)
     play.runAndWait()
-    
+
     
